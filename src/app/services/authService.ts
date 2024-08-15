e2fd6e1ec0371aba88399f8d96ecdce3864ab9e9
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-15 11:50:11 +0700

    Update authService.ts

--- src/app/services/authService.ts
+++ src/app/services/authService.ts
@@ -1,26 +1,8 @@
-import { SignInResponse } from "../types/auth";
 import { API_URL } from "../utils/api";
 
-// export const signInService = async (email: string, password: string) => {
-//   const url = API_URL("signIn");
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ email, password }),
-//   });
-
-//   if (!response.ok) {
-//     const errorData = await response.json();
-//     throw new Error(errorData.message || "Sign In failed");
-//   }
-
-//   return await response.json();
-// };
-
 export const signInService = async (email: string, password: string) => {
-  const url = "http://localhost:3010/api/signIn";
+  // const url = "http://localhost:3010/api/signIn";
+  const url = API_URL("signIp");
   const response = await fetch(url, {
     method: "POST",
     headers: {
